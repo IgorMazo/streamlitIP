e3d989cb1eb9d802a533fc443135e848c133c00a
IgorMazo <igorgmmazo@gmail.com>
2024-07-03 12:12:41 -0300

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title('Meu Primeiro Aplicativo Streamlit')
+    st.write("Aqui estou escrevendo meu primeiro aplicativo Streamlit!")
+
+if __name__ == '__main__':
+    main()
